c1835d021cb425b22990d2f358c2f18ae02ab52c
Philip Scott <pscott@zeptohost.com>
2019-10-12 00:51:51 -0400

    add viper config

--- cmd/generator/generator.go
+++ cmd/generator/generator.go
@@ -2,7 +2,10 @@ package generator
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
+
+	"github.com/spf13/viper"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/lumina-tech/gooq/pkg/database"
@@ -10,23 +13,31 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var generateDatabaseModelCommandUseDocker bool
+var (
+	generateDatabaseModelCommandUseDocker bool
+	generateDatabaseModelConfigFilePath   string
+)
+
 var generateDatabaseModelCommand = &cobra.Command{
 	Use:   "generate-database-model",
 	Short: "generate Go models by introspecting the database",
 	Run: func(cmd *cobra.Command, args []string) {
-
+		err := initConfig()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Cannot read configuration file:", err)
+			os.Exit(1)
+		}
 		config := database.DatabaseConfig{
-			Host:          "localhost",
-			Port:          5432,
-			Username:      "swapi",
-			Password:      "swapi",
-			Version:       "11.4",
-			DatabaseName:  "swapi",
-			SSLMode:       "disable",
-			MigrationPath: "examples/swapi/migrations",
-			ModelPath:     "examples/swapi/model",
-			TablePath:     "examples/swapi/table",
+			Host:          viper.GetString("host"),
+			Port:          viper.GetInt64("port"),
+			Username:      viper.GetString("username"),
+			Password:      viper.GetString("password"),
+			Version:       viper.GetString("version"),
+			DatabaseName:  viper.GetString("databaseName"),
+			SSLMode:       viper.GetString("disable"),
+			MigrationPath: viper.GetString("migrationPath"),
+			ModelPath:     viper.GetString("modelPath"),
+			TablePath:     viper.GetString("tablePath"),
 		}
 		if generateDatabaseModelCommandUseDocker {
 			db := database.NewDockerizedDB(&config)
@@ -38,6 +49,23 @@ var generateDatabaseModelCommand = &cobra.Command{
 			generateModelsForDB(db, &config)
 		}
 	},
+}
+
+func initConfig() error {
+	if len(generateDatabaseModelConfigFilePath) != 0 {
+		viper.SetConfigFile(generateDatabaseModelConfigFilePath)
+		return viper.ReadInConfig()
+	}
+
+	viper.SetConfigName("gooq")
+
+	wd, err := os.Getwd()
+	if err != nil {
+		wd = "."
+	}
+	viper.AddConfigPath(wd)
+
+	return viper.ReadInConfig()
 }
 
 func generateModelsForDB(
@@ -54,5 +82,4 @@ func generateModelsForDB(
 	tableTemplatePath := filepath.Join(templateDir, generator.TableTemplateFilename)
 	generatedTableFilename := fmt.Sprintf("%s/%s_table.generated.go", config.TablePath, config.DatabaseName)
 	generator.GenerateModel(db, tableTemplatePath, generatedTableFilename, config.DatabaseName)
-
 }
