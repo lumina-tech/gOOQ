a6d42588036931f8746c1941cad5ea556e0c1dc4
Peter Pong <peterpong@bitgo.com>
2021-02-25 16:09:37 -0800

    Fix insert multiple values

--- pkg/gooq/insert_test.go
+++ pkg/gooq/insert_test.go
@@ -15,14 +15,14 @@ var insertTestCases = []TestCase{
 		Constructed: InsertInto(Table1).
 			Values("1", "2", 3, 4).
 			Values("2", "3", 4, 5),
-		ExpectedStmt: `INSERT INTO public.table1 VALUES ($1, $2, $3, $4) ($5, $6, $7, $8)`,
+		ExpectedStmt: `INSERT INTO public.table1 VALUES ($1, $2, $3, $4), ($5, $6, $7, $8)`,
 	},
 	{
 		Constructed: InsertInto(Table1).
 			Columns(Table1.Column1, Table1.Column2, Table1.Column3, Table1.Column4).
 			Values("1", "2", 3, 4).
 			Values("2", "3", 4, 5),
-		ExpectedStmt: `INSERT INTO public.table1 (column1, column2, column3, column4) VALUES ($1, $2, $3, $4) ($5, $6, $7, $8)`,
+		ExpectedStmt: `INSERT INTO public.table1 (column1, column2, column3, column4) VALUES ($1, $2, $3, $4), ($5, $6, $7, $8)`,
 	},
 	{
 		Constructed:  InsertInto(Table1).Select(Select(Table1.Column1).From(Table1)),
